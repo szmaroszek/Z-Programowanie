312a7fde77fd1d2d4e14363bf82848c65c1e4506
szmaroszek <szmaroszek@gmail.com>
2021-11-21 17:56:53 +0100

    Add lab_4

---
+++ zad_2.py
@@ -0,0 +1,45 @@
+class Library:
+    def __init__(self, city, street, zip_code, open_hours, phone):
+        self.city = city
+        self.street = street
+        self.zip_code = zip_code
+        self.open_hours = open_hours
+        self.phone = phone
+
+    def __str__(self):
+        return f'Library in {self.city}, {self.zip_code} open hours: {self.open_hours}.'
+
+class Order:
+    def __init__(self, employee, student, books, order_date):
+        self.employee = employee
+        self.student = student
+        self.books = books
+        self.order_date = order_date
+
+    def __str__(self):
+        return f'Books {self.books} ordered by {self.student} and processed by {self.employee} on {self.order_date}.'
+
+class Employee:
+    def __init__(self, first_name, last_name, hire_date, birth_date, city, street, zip_code, phone):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.hire_date = hire_date
+        self.birth_date = birth_date
+        self.city = city
+        self.street = street
+        self.zip_code = zip_code
+        self.phone = phone
+
+    def __str__(self):
+        return f'Employee {self.first_name} {self.last_name}, phone: {self.phone}.'
+
+class Book:
+    def __init__(self, library, publication_date, author_name, author_surname, number_of_pages):
+        self.library = library
+        self.publication_date = publication_date
+        self.author_name = author_name
+        self.author_surname = author_surname
+        self.number_of_pages = number_of_pages
+
+    def __str__(self):
+        return f'Book from {self.library} library, publication date {self.publication_date} from {self.author_name}.'
