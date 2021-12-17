c29ec9bb76c3d3bbe516c90ef89ee5f2bdcd4277
Szymon Maroszek <Szymon Maroszek>
2021-12-17 23:54:46 +0100

    Finish task 2, Add task 3

--- zad_2.py
+++ zad_2.py
@@ -17,7 +17,7 @@ class Order:
         self.order_date = order_date
 
     def __str__(self):
-        return f'Books {self.books} ordered by {self.student} and processed by {self.employee} on {self.order_date}.'
+        return f'Order date: {self.order_date}, Books:\n' + '\n'.join(str(book) for book in self.books) + f'\nOrdered by student - {self.student}, processed by {self.employee} on {self.order_date}.' + '\n'
 
 class Employee:
     def __init__(self, first_name, last_name, hire_date, birth_date, city, street, zip_code, phone):
@@ -33,7 +33,7 @@ class Employee:
     def __str__(self):
         return f'Employee {self.first_name} {self.last_name}, phone: {self.phone}.'
 
-class Book:
+class Book():
     def __init__(self, library, publication_date, author_name, author_surname, number_of_pages):
         self.library = library
         self.publication_date = publication_date
@@ -42,4 +42,24 @@ class Book:
         self.number_of_pages = number_of_pages
 
     def __str__(self):
-        return f'Book from {self.library} library, publication date {self.publication_date} from {self.author_name}.'
+        return f'Book from {self.library} Publication date {self.publication_date} from {self.author_name}.'
+
+
+library_1 = Library('Katowice', 'Hallera', '50-211', '10:00 - 17:00', '868686868')
+library_2 = Library('Katowice', 'Gospodarcza', '50-215', '11:00 - 19:00', '686868686')
+
+book_1 = Book(library_1, 2018, 'Rodney', 'Scott', 25)
+book_2 = Book(library_1, 2017, 'Rodne', 'S', 250)
+book_3 = Book(library_2, 2016, 'Rodn', 'Sc', 2500)
+book_4 = Book(library_1, 2015, 'Rod', 'Sco', 25000)
+book_5 = Book(library_2, 2014, 'Ro', 'Scot', 25000)
+
+employee_1 = Employee('Szymon', 'Maroszek', 2019, '21-08-2010', 'Katowice', 'Gospodarcza', '10-201', '999999999')
+employee_2 = Employee('Szymon2', 'Maroszek3', 2020, '22-09-2011', 'Sosnowiec', 'Hallera', '15-201', '999994999')
+employee_3 = Employee('Szymon3', 'Maroszek3', 2021, '23-09-2013', 'Radom', 'Galla', '10-209', '999933999')
+
+order_1 = Order(employee_1, True, [book_1, book_2], '22-10-2022')
+order_2 = Order(employee_3, False, [book_4, book_3, book_5], '22-11-2022')
+
+print(order_1)
+print(order_2)
---
+++ zad_3.py
@@ -0,0 +1,26 @@
+class Property:
+    def __init__(self, area: str, rooms: int, price: float, adress: str):
+        self.area = area
+        self.rooms = rooms
+        self.price = price
+        self.address = adress
+
+class House(Property):
+    def __init__(self, area, rooms, price, adress, plot: int):
+        super().__init__(area, rooms, price, adress)
+        self.plot = plot
+    def __str__(self):
+        return f'House located in {self.area} area, {self.rooms} rooms, {self.plot} m^2, for a price of ${self.price} USD.\n'
+
+class Flat(Property):
+    def __init__(self, area, rooms, price, adress, floor):
+        super().__init__(area, rooms, price, adress)
+        self.floor = floor
+    def __str__(self):
+        return f'Flat located in {self.area} area, {self.rooms} rooms on floor: {self.floor}, for a price of ${self.price} USD.\n'
+
+house_1 = House('Bay', 5, 119000.0, "5th Street, Los Angeles", 100)
+flat_1 = Flat('Center', 3, 219000.0, "15th Street, Los Angeles", 50)
+
+print(house_1)
+print(flat_1)
