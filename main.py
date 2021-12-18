d5f8569ea60881a9602b2154007b9b32f5f12c58
Szymon Maroszek <Szymon Maroszek>
2021-12-18 16:20:55 +0100

    fix formatting

--- main.py
+++ main.py
@@ -25,17 +25,20 @@ img_rgb = cv2.cvtColor(img_1_cv, cv2.COLOR_BGR2GRAY)
 
 filtered_img_1 = []
 filtered_img_1.append(cv2.threshold(cv2.GaussianBlur(img_rgb, (5, 5), 0), 0, 255,
-                              cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1])
+                                    cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1])
 filtered_img_1.append(cv2.threshold(cv2.bilateralFilter(img_rgb, 5, 75, 75), 0, 255,
                                     cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1])
 filtered_img_1.append(cv2.threshold(cv2.medianBlur(img_rgb, 3), 0, 255,
                                     cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1])
 filtered_img_1.append(cv2.adaptiveThreshold(cv2.GaussianBlur(img_rgb, (5, 5), 0), 255,
-                      cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2))
+                                            cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                            cv2.THRESH_BINARY, 31, 2))
 filtered_img_1.append(cv2.adaptiveThreshold(cv2.bilateralFilter(img_rgb, 9, 75, 75), 255,
-                      cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2))
+                                            cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                            cv2.THRESH_BINARY, 31, 2))
 filtered_img_1.append(cv2.adaptiveThreshold(cv2.medianBlur(img_rgb, 3), 255,
-                      cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2))
+                                            cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                            cv2.THRESH_BINARY, 31, 2))
 
 filtered_photos_read = []
 for photo in filtered_img_1:
